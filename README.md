70267d22d8897b05092cdc9facf96dfb770ee1ca
heiheiya <122619445@qq.com>
2016-01-24 15:43:16 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+
+This is Getting and Cleaning Data Course Project.
+
+The purpose is creating a new tidy data set with the average of each variable for each activity and each subject from the raw data.
+
+The data set is download from: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+In the script run_analysis.R, I
+
+1.Merges the training and the test sets to create one data set.
+
+2.Extracts only the measurements on the mean and standard deviation for each measurement.
+
+3.Uses descriptive activity names to name the activities in the data set.
+
+4.Appropriately labels the data set with descriptive variable names.
+
+4.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+You can find more information about the data in CodeBook.md.
+
+Run run_analysis.R or source it. It will generate a text file named "tidtData.txt". This is the required data set.
---
+++ run_analysis.R
@@ -0,0 +1,58 @@
+#read data from files
+train_x<-read.table("./UCI HAR Dataset/train/X_train.txt")
+train_y<-read.table("./UCI HAR Dataset/train/y_train.txt")
+subject_train<-read.table("./UCI HAR Dataset/train/subject_train.txt")
+
+test_x<-read.table("./UCI HAR Dataset/test/X_test.txt")
+test_y<-read.table("./UCI HAR Dataset/test/y_test.txt")
+subject_test<-read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+activity_labels<-read.table("./UCI HAR Dataset/activity_labels.txt")
+features<-read.table("./UCI HAR Dataset/features.txt")
+
+#combine train_x dataset and test_x dataset to a new dataset
+newData_x<-rbind(train_x,test_x)
+
+#use features to name the newData_x columns
+colnames(newData_x) <- c(as.character(features[,2]))
+
+#find mean and std,learn from slides 4-1 Editing Text Variables 
+mean<-grep("mean()",colnames(newData_x),fixed=TRUE)
+
+sd<-grep("std()",colnames(newData_x),fixed=TRUE)
+
+mean_sd<-newData_x[,c(mean,sd)]
+
+#combine train_y dataset and test_y dataset to a new dataset
+newData_y<-rbind(train_y,test_y)
+
+newActivity<-cbind(newData_y,mean_sd)
+
+#name the Activity
+colnames(newActivity)[1] <- "Activity"
+
+#convert to character
+activity_labels[,2]<-as.character(activity_labels[,2])
+
+activity_len <- length(newActivity[,1])
+for(i in 1:activity_len){
+  newActivity[i,1]<-activity_labels[newActivity[i,1],2]
+}
+
+#combine subject dataset
+newSubject<-rbind(subject_train,subject_test)
+
+newSubAct<-cbind(newSubject,newActivity)
+
+colnames(newSubAct)[1] <- "Subject"
+
+tidyData <- aggregate( newSubAct[,3] ~ Subject+Activity, data = newSubAct, FUN= "mean" )
+
+newSubAct_col <- ncol(newSubAct)
+for(i in 4:newSubAct_col){
+  tidyData[,i] <- aggregate( newSubAct[,i] ~ Subject+Activity, data = newSubAct, FUN= "mean" )[,3]
+}
+
+colnames(tidyData)[3:ncol(tidyData)] <- colnames(mean_sd)
+
+write.table(tidyData, file = "tidyData.txt")
